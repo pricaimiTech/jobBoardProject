4ffbe2f4b4916a597d89e9816cbef90a82b3cd58
Priscila Caimi <priscila.caimi@govoll.com>
2024-10-21 14:13:23 -0300

    tratamento de erros

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,25 +1,44 @@
-import { getCompany } from "./db/companies.js"
-import { getJob, getJobs, getJobsByCompany } from "./db/jobs.js"
-import { getUser } from "./db/users.js"
+import { GraphQLError } from "graphql";
+import { getCompany } from "./db/companies.js";
+import { getJob, getJobs, getJobsByCompany } from "./db/jobs.js";
+import { getUser } from "./db/users.js";
 
 export const resolvers = {
-    Query: {
-        jobs: () => getJobs(),
-        users: () => getUser(),
-        job: (_root, {id}) => getJob(id),
-        company: (_root, {id}) => getCompany(id)
+  Query: {
+    jobs: () => getJobs(),
+    users: () => getUser(),
+    job: async (_root, { id }) => {
+      const job = await getJob(id);
+      if (!job) {
+        throw notFoundErro("No Job found with id " + id);
+      }
+      return job;
     },
-
-    Job: {
-        company: (job) => getCompany(job.companyId),
-        createdAt: (job) => toIsoDate(job.createdAt)
+    company: async (_root, { id }) => {
+      const company = await getCompany(id);
+      if (!company) {
+        throw notFoundErro("No company found with id " + id);
+      }
+      return company;
     },
+  },
 
-    Company: {
-        jobs: (company) => getJobsByCompany(company.id)
-    }
-}
+  Job: {
+    company: (job) => getCompany(job.companyId),
+    createdAt: (job) => toIsoDate(job.createdAt),
+  },
+
+  Company: {
+    jobs: (company) => getJobsByCompany(company.id),
+  },
+};
 
 function toIsoDate(date) {
-    return date.slice(0, 'dd-mm-yyyy'.length)
+  return date.slice(0, "dd-mm-yyyy".length);
+}
+
+function notFoundErro(message) {
+  return new GraphQLError(message, {
+    extensions: { code: "NOT_FOUND" },
+  });
 }
